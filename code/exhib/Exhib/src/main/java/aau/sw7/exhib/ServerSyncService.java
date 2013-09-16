38a6a7957164ba4633e4d6f26299af806aaa4410
figa12 <figa.figa@gmail.com>
2013-09-16 13:08:16 +0200

    New class. and some test code for server interaction.

---
+++ code/exhib/Exhib/src/main/java/aau/sw7/exhib/ServerSyncService.java
@@ -0,0 +1,17 @@
+package aau.sw7.exhib;
+
+import android.os.AsyncTask;
+
+import java.net.URL;
+
+/**
+ * Created by figa on 9/16/13.
+ */
+public class ServerSyncService extends AsyncTask<URL, Integer, Long> {
+
+
+    @Override
+    protected Long doInBackground(URL... params) {
+        return null;
+    }
+}
