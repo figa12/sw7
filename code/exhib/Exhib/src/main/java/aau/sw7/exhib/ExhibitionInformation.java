920cc72ab8710f01a5bb42a99eae579cb7a07264
Henrik <henrikklarup@gmail.com>
2013-09-25 23:45:36 +0200

    Changing to fragments

--- code/exhib/Exhib/src/main/java/aau/sw7/exhib/ExhibitionInformation.java
+++ code/exhib/Exhib/src/main/java/aau/sw7/exhib/ExhibitionInformation.java
@@ -1,13 +1,27 @@
 package aau.sw7.exhib;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by jerian on 16-09-13.
  */
-public class ExhibitionInformation extends Activity {
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+public class ExhibitionInformation extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstance)
+    {
+        super.onCreate(savedInstance);
     }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View v = inflater.inflate(R.layout.activity_exhibition_information, container,false);
+        return v;
+    }
+
 }
