575a62dc07a28829edb458a2c650ae4e19f20257
Henrik <henrikklarup@gmail.com>
2013-09-30 13:18:53 +0200

    Reverted Changes

--- code/exhib/Exhib/src/main/java/aau/sw7/exhib/ExhibitionInformation.java
+++ code/exhib/Exhib/src/main/java/aau/sw7/exhib/ExhibitionInformation.java
@@ -1,27 +1,15 @@
 package aau.sw7.exhib;
 
 import android.app.Activity;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 /**
  * Created by jerian on 16-09-13.
  */
-public class ExhibitionInformation extends Fragment {
+public class ExhibitionInformation extends Activity {
     @Override
-    public void onCreate(Bundle savedInstance)
+    public void onCreate(Bundle savedInstanceState)
     {
-        super.onCreate(savedInstance);
+        super.onCreate(savedInstanceState);
     }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-    {
-        View v = inflater.inflate(R.layout.activity_exhibition_information, container,false);
-        return v;
-    }
-
 }
